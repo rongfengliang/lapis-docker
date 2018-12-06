fe56a45c3fd04923be4e6645af4b6f697701f526
dalongrong <1141591465@qq.com>
2018-12-06 09:21:12 +0800

    add more metrics path for test

--- web/app.lua
+++ web/app.lua
@@ -5,4 +5,25 @@ app:get("/", function()
   return "Welcome to Lapis  dalongrong " .. require("lapis.version")
 end)
 
+app:get("/users", function()
+  return "users" .. require("lapis.version")
+end)
+
+app:get("/apps", function()
+  return "apps" .. require("lapis.version")
+end)
+
+
+app:get("/mobiles", function()
+  return "mobiles" .. require("lapis.version")
+end)
+
+
+app:get("/paas", function()
+  return "paas " .. require("lapis.version")
+end)
+app:get("/saas", function()
+  return "saas " .. require("lapis.version")
+end)
+
 return app
